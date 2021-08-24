f76655297963196e912aedf3fc1c1ec346d6af45
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-08-24 12:37:03 +0900

    Feat: local storage 유틸 클래스 추가

---
+++ src/utils/storage/generator.ts
@@ -0,0 +1,20 @@
+export default class Generator {
+  private readonly key: string;
+
+  constructor(key: string) {
+    this.key = key;
+    !this.load() && this.save();
+  }
+
+  save<T>(data: T | null = null): void {
+    return localStorage.setItem(this.key, JSON.stringify(data));
+  }
+
+  load() {
+    return JSON.parse(localStorage.getItem(this.key) as string);
+  }
+
+  remove(): void {
+    localStorage.removeItem(this.key);
+  }
+}
