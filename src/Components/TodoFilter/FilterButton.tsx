5147b2db544e2d75815364844cc6e060c91bb8da
Seongsangcho <liasqui31@gmail.com>
2021-08-27 17:05:04 +0900

    style : TodoFilter figma 시안 반영
    Co-authored-by: kimjy392 <kimjy392@users.noreply.github.com>
    Co-authored-by: Jiyeol Jake Lee <highspirit7@users.noreply.github.com>
    Co-authored-by: jtwjs <jtwjs@users.noreply.github.com>

--- src/Components/TodoFilter/FilterButton.tsx
+++ src/Components/TodoFilter/FilterButton.tsx
@@ -20,18 +20,21 @@ const FilterButton: React.FC<IFilterButtonProps> = ({
 };
 
 const StyledFilterButton = styled(Button)<IFilterButtonProps>`
-  border: 1px solid ${({ theme }) => theme.colors.orange};
-  border-radius: 3px;
-  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 8px;
+  padding: 7px 12px;
+  font-size: 14px;
+  min-width: 34px;
   font-weight: bold;
+  margin-left: 8px;
   ${({ toggleClick, theme }) =>
     toggleClick
       ? css`
           color: ${theme.colors.white};
-          background-color: ${theme.colors.orange};
+          background-color: ${theme.colors.primary};
         `
       : css`
-          color: ${theme.colors.orange};
+          color: ${theme.colors.primary};
           background-color: transparent;
         `};
 `;
